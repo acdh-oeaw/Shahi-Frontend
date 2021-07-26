d56484d5e56c7892ee76771113eeefadb45b0ba9
schnoeggi <andreas@olschnoegger.at>
2021-07-26 11:00:38 +0200

    fix typo

--- store/app.js
+++ store/app.js
@@ -3,7 +3,7 @@ import classes from '../assets/classes.json';
 import tableheaders from '../assets/tableheaders.json';
 import templateprops from '../assets/templateprops.json';
 import filterelements from '../assets/filterElements.json';
-import viewelements from '../assets/viewelements.json';
+import viewelements from '../assets/viewElements.json';
 
 /* eslint-disable no-param-reassign,no-shadow */
 export const state = () => ({
