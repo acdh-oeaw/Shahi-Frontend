d7085964cb0559949f9b7771e427fb0dcd77cfb4
Andi <andreas@olschnoegger.at>
2021-09-06 17:34:08 +0200

    make searchBar responsive

--- components/querysearchNew.vue
+++ components/querysearchNew.vue
@@ -1,8 +1,7 @@
 <template>
-
   <v-combobox
     v-model="filterArray"
-
+    single-line
     solo-inverted
     flat
     hide-details
@@ -13,24 +12,21 @@
     @click="open = !open"
   >
     <template v-slot:append>
-      <FilterWindow :global-search="globalSearch" :open-window="open"/>
+      <FilterWindow :global-search="globalSearch" :open-window="open" />
     </template>
     <template v-slot:selection="{ attrs, item, parent, selected }">
-
       <v-chip
         v-if="item === Object(item)"
         class="text-capitalize"
         label
         small
-
       >
-
-        <span class="text-body-1" v-if="item.value !== true & item.value !== false">
-            {{item.value}}
+        <span v-if="item.value !== true & item.value !== false" class="text-body-1">
+          {{ item.value }}
         </span>
 
-        <span class="text-body-1" v-else>
-            {{item.en}}
+        <span v-else class="text-body-1">
+          {{ item.en }}
         </span>
         <v-icon
           v-if="!item.codes"
@@ -42,91 +38,92 @@
         </v-icon>
       </v-chip>
     </template>
-
-
   </v-combobox>
 </template>
 
 <script>
 import FilterWindow from '@/components/FilterWindow';
-import {mapGetters} from "vuex";
+import { mapGetters } from 'vuex';
+
 export default {
   components: { FilterWindow },
   data() {
     return {
       open: false,
       globalSearch: '',
-      filterstring: '',
+      filterString: '',
       filterArray: [],
     };
   },
-  mounted(){
-    this.updateArray();
+  computed: {
+    ...mapGetters('app', [
+      'getFilterObject',
+      'getCurrentFilters',
+    ]),
   },
   watch: {
     '$store.state.app.filterelements': {
       handler() {
-        console.log("wurde geändert")
         this.updateArray();
       },
       immediate: true,
       deep: true,
     },
-    '$route.query':{
-      handler(){
+    '$route.query': {
+      handler() {
         this.globalSearch = '';
       },
-      immediate:true,
-      deep:true,
+      immediate: true,
+      deep: true,
     },
+  },
+  mounted() {
+    this.updateArray();
   },
   methods: {
     updateString(query) {
       if (!query) {
-        this.filterstring = '';
+        this.filterString = '';
       } else {
-        this.filterstring = query;
+        this.filterString = query;
       }
     },
     updateArray() {
-      this.filterArray = this.getCurrentFilters?.items.map((x, index) => {
-        const filters = x.values.map((v) => {
-          v.group = index;
-          return v;
-        });
-        return filters;
-      })
+      this.filterArray = this.getCurrentFilters?.items.map((x, index) => x.values.map((v) => {
+        // eslint-disable-next-line no-param-reassign
+        v.group = index;
+        return v;
+      }))
         .flat()
         .filter((x) => x.value);
-
-      console
-        .log(this.filterArray);
     },
 
     updateFilter(selectedProperty, value) {
-
-      console.log(selectedProperty)
-      console.log(value)
       this.$store.commit('app/updateFilterValue', {
         selectedProperty,
-        value
+        value,
       });
-      const name = this.$route.name
-
-
+      const { name } = this.$route;
       this.$router.push({
         name,
         query: this.getFilterObject,
-
       });
     },
-  },
-    computed: {
-    ...mapGetters('app', [
-      'getFilterObject',
-      'getCurrentFilters'
-    ]),
   },
 
 };
 </script>
+
+<style>
+.v-select__slot .v-select__selections {
+
+  height: 50px;
+  overflow: auto;
+  -ms-overflow-style: none; /* for Internet Explorer, Edge */
+  scrollbar-width: none; /* for Firefox */
+}
+
+.v-select__slot ::-webkit-scrollbar {
+  display: none; /* for Chrome, Safari, and Opera */
+}
+</style>
