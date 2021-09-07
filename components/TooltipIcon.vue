c13cce355a8fcfc79984ff4bd958e25bb407a826
Andi <andreas@olschnoegger.at>
2021-09-07 13:50:34 +0200

    make own tooltip component

---
+++ components/TooltipIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-tooltip
+
+    right
+    max-width="400px"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-icon
+        class="ml-1"
+        :color="color"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <slot> mdi-help-circle-outline</slot>
+      </v-icon>
+    </template>
+    {{ text }}
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'TooltipIcon',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: undefined,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
