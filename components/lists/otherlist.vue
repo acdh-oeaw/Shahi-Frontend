26bc8f528b63119f4966a5a428872536235c1e10
Andi <andreas@olschnoegger.at>
2021-09-07 10:19:38 +0200

    change style and order of list

--- components/lists/otherlist.vue
+++ components/lists/otherlist.vue
@@ -1,145 +1,172 @@
 <template>
   <div class="mt-4">
-    <p class="text-body-1 ma-5" v-if="notFound">No records found. </p>
+    <p v-if="notFound" class="text-body-1 ma-5">
+      No records found.
+    </p>
 
     <v-card
       v-for="(item, index) in items"
       :key="index"
       outlined
-      rounded="lg"
-      class="ma-2 mr-5"
+      max-width="1500px"
+      class="ma-5"
       style="overflow: hidden"
     >
       <v-row no-gutters>
-        <v-col cols="12" sm="3" md="2">
-          <v-img
-            max-width="100%"
-            :src="
-              'http://localhost:8182/iiif/3/' +
-              ['sculpture', 'plate'][Math.floor(Math.random() * 2)] +
-              '1.jpg/full/400,/0/default.jpg'
-            "
-          ></v-img>
-        </v-col>
-        <v-col cols="12" sm="9" md="10">
-            <nuxt-link
-                :to="`/single/${
-                  item.features[0]['@id'].split('/').splice(-1)[0]
-                }`"
+        <v-col cols="12">
+          <nuxt-link
+            :to="`/single/${
+              item.features[0]['@id'].split('/').splice(-1)[0]
+            }`"
+          >
+            <v-hover v-slot="{hover}">
+              <v-card-title
+                class="artifact-title grey d-block white--text"
+                :class="hover ? 'lighten-1' : ''"
               >
-              <v-hover v-slot="{hover}">
-          <v-card-title class="artifact-title grey d-block white--text " :class="hover ? 'lighten-1' : ''">
-            <p class="ma-0 pa-0">
-
-                {{ item.features[0].properties.title }}
-            </p>
-            <p class="text-caption pa-0 ma-0">
-              {{getFirstTypeByKeyword(item.features[0].types,'Artifact').label}}
-              <span v-if="!!getFirstTypeByKeyword(item.features[0].types,'Artifact').subtype">({{getFirstTypeByKeyword(item.features[0].types,'Artifact').subtype}})</span>
-            </p>
-            <p class="text-caption pa-0 ma-0">
-              <span v-if="item.features[0].when.timespans[0].start.earliest">
-                from
-                {{
-                  item.features[0].when.timespans[0].start.earliest
-                    .split("-")[0]
-                    .replace(/^0+/, "")
-                }}
-              </span>
-
-              <span
-                v-if="
-                  item.features[0].when.timespans[0].end.latest ||
-                  item.features[0].when.timespans[0].end.earliest
-                "
-                >to
-                {{
-                  item.features[0].when.timespans[0].end.latest
-                    ? item.features[0].when.timespans[0].end.latest
+                <p class="ma-0 pa-0">
+                  {{ item.features[0].properties.title }}
+                </p>
+                <p class="text-caption pa-0 ma-0">
+                  {{ getFirstTypeByKeyword(item.features[0].types, 'Artifact').label }}
+                  <span
+                    v-if="!!getFirstTypeByKeyword(item.features[0].types,'Artifact').subtype"
+                  >({{ getFirstTypeByKeyword(item.features[0].types, 'Artifact').subtype }})</span>
+                </p>
+                <p class="text-caption pa-0 ma-0">
+                  <span v-if="item.features[0].when.timespans[0].start.earliest">
+                    from
+                    {{
+                      item.features[0].when.timespans[0].start.earliest
                         .split("-")[0]
                         .replace(/^0+/, "")
-                    : item.features[0].when.timespans[0].end.earliest
-                        .split("-")[0]
-                        .replace(/^0+/, "")
-                }}
-              </span>
-            </p>
-          </v-card-title>
-          </v-hover>
-              </nuxt-link>
+                    }}
+                  </span>
 
-          <v-card-text class="text-body-1">
-            <v-card outlined class="my-3" v-if="item.features[0].description">
-              <v-card-text class="text-body-1">
-                Category of Authenticity:    <nuxt-link
-                :to='`/detaillist?codes=artifact&type_id=${getFirstTypeByKeyword(item.features[0].types,"Category of Authenticity").identifier.split("/").splice(-1)[0]}`'
-              >{{getFirstTypeByKeyword(item.features[0].types,'Category of Authenticity').label}}</nuxt-link>
-              </v-card-text>
-              <v-card-text class="text-body-1">{{
-                item.features[0].description[0].value
-              }}</v-card-text>
-            </v-card>
-            <div class="card-columns" :style="cssVars">
-              <div
-                v-if="typeGroup[0].type !== 'Artifact' && typeGroup[0].type !== 'Category of Authenticity'"
-                class="card-column"
-                v-for="(typeGroup, index) in getOrderedTypes(
-                  item.features[0].types
-                )"
-                :key="index + 2000"
+                  <span
+                    v-if="
+                      item.features[0].when.timespans[0].end.latest ||
+                        item.features[0].when.timespans[0].end.earliest
+                    "
+                  >to
+                    {{
+                      item.features[0].when.timespans[0].end.latest
+                        ? item.features[0].when.timespans[0].end.latest
+                          .split("-")[0]
+                          .replace(/^0+/, "")
+                        : item.features[0].when.timespans[0].end.earliest
+                          .split("-")[0]
+                          .replace(/^0+/, "")
+                    }}
+                  </span>
+                </p>
+              </v-card-title>
+            </v-hover>
+          </nuxt-link>
+          <v-row no-gutters>
+            <v-col cols="12" sm="3" md="2">
+              <v-card
+                height="100%"
+                rounded="0"
+                class="d-flex align-center pa-sm-2"
+                :class="{'borderBottom': $vuetify.breakpoint.xs}"
+                elevation="0"
+                color="grey lighten-2"
               >
-                <v-card class="mb-5" outlined>
-                  <v-card-title
-                    class="text-subtitle-2 justify-center grey lighten-3"
-                    >{{ typeGroup[0].type }}
-                  </v-card-title>
-                  <v-card-text>
-                    <p
-                      v-for="(type, index) in typeGroup"
-                      :key="index"
-                      class="ma-0 ml-5 pa-0"
+                <v-img
+
+                  max-width="100%"
+                  :src="
+                    'http://localhost:8182/iiif/3/' +
+                      ['sculpture', 'plate'][Math.floor(Math.random() * 2)] +
+                      '1.jpg/full/400,/0/default.jpg'
+                  "
+                  alt="IMAGE"
+                />
+              </v-card>
+            </v-col>
+            <v-col cols="12" sm="9" md="10" class="px-5">
+              <v-card v-if="item.features[0].description" outlined class="my-3">
+                <v-card-text class="text-body-1">
+                  Category of Authenticity:
+                  <nuxt-link
+                    :to="`/detaillist?codes=artifact&type_id=${getFirstTypeByKeyword(item.features[0].types,'Category of Authenticity').identifier.split(';').splice(-1)[0]}`"
+                  >
+                    {{ getFirstTypeByKeyword(item.features[0].types, 'Category of Authenticity').label }}
+                  </nuxt-link>
+                </v-card-text>
+                <v-card-text class="text-body-1">
+                  {{
+                    item.features[0].description[0].value
+                  }}
+                </v-card-text>
+              </v-card>
+              <div class="card-columns" :style="cssVars">
+                <div
+                  v-for="(typeGroup, i) in getOrderedTypes(
+                    item.features[0].types
+                  )"
+                  v-if="typeGroup[0].type !== 'Artifact' && typeGroup[0].type !== 'Category of Authenticity'"
+                  :key="i + 2000"
+                  class="card-column"
+                >
+                  <v-card class="mb-5" outlined>
+                    <v-card-title
+                      class="text-subtitle-2 justify-center grey lighten-3"
                     >
-                      <nuxt-link
-                                 :to='`/detaillist?codes=artifact&type_id=${type.id}`'
+                      {{ typeGroup[0].type }}
+                    </v-card-title>
+                    <v-card-text>
+                      <p
+                        v-for="(type, idx) in typeGroup"
+                        :key="idx"
+                        class="ma-0 ml-5 pa-0"
                       >
-                        {{ type.label }}
-                      </nuxt-link>
-                      <span v-if="!!type.value"
-                        >: {{ type.value }} {{ type.unit }}</span
-                      >
-                    </p>
-                  </v-card-text>
-                </v-card>
+                        <nuxt-link
+                          :to="`/detaillist?codes=artifact&type_id=${type.id}`"
+                        >
+                          {{ type.label }}
+                        </nuxt-link>
+                        <span
+                          v-if="!!type.value"
+                        >: {{ type.value }} {{ type.unit }}</span>
+                      </p>
+                    </v-card-text>
+                  </v-card>
+                </div>
               </div>
-            </div>
-          </v-card-text>
-        </v-col></v-row
-      >
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
     </v-card>
 
     <v-pagination
       v-model="options.page"
       :length="Math.floor(totalItems / options.itemsPerPage)"
       :total-visible="7"
-    ></v-pagination>
+    />
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex';
 
 export default {
   props: {
     filter: {
       type: Object,
-      default: () => {},
+      default: () => {
+      },
     },
   },
   async fetch() {
     this.loading = true;
     this.notFound = false;
     this.items = [];
-    const { sortBy, sortDesc, page, itemsPerPage } = this.options;
+    const {
+      sortBy, sortDesc, page, itemsPerPage,
+    } = this.options;
     try {
       // eslint-disable-next-line no-underscore-dangle
       const p = await this.$api.Entities.get_api_0_2_query_({
@@ -147,14 +174,13 @@ export default {
         first: this.itemIndex[page - 1] ? this.itemIndex[page - 1].startId : null,
         filter: this.filter,
         column: sortBy ? this.getSortColumnByPath(sortBy[0]) : null,
-        sort: sortDesc[0] ? "desc" : "asc",
+        sort: sortDesc[0] ? 'desc' : 'asc',
       });
       // eslint-disable-next-line prefer-destructuring
-      console.log(p.body);
       this.items = p.body.results;
       this.itemIndex = p.body.pagination.index;
       this.totalItems = p.body.pagination.entities;
-    } catch(err) {
+    } catch (err) {
       this.notFound = true;
     }
     this.loading = false;
@@ -176,11 +202,29 @@ export default {
       itemIndex: [],
     };
   },
+  computed: {
+    ...mapGetters('app', [
+      'getTypesBySystemClass',
+      'getIconBySystemClass',
+      'getLabelBySystemClass',
+      'getCRMClassBySystemClass',
+      'getSortColumnByPath',
+      'getSystemClassForFilter',
+      'getFilterList',
+    ]),
+    cssVars() {
+      let colCount = 2;
+      if (this.$vuetify.breakpoint.name === 'xs') colCount = 1;
+
+      return {
+        '--column-count': colCount,
+      };
+    },
+  },
   watch: {
     options: {
       handler(o, n) {
-        if (o.sortBy !== n.sortBy || o.sortDesc !== n.sortDesc)
-          this.itemIndex = [];
+        if (o.sortBy !== n.sortBy || o.sortDesc !== n.sortDesc) this.itemIndex = [];
         this.$fetch();
       },
       deep: true,
@@ -188,8 +232,8 @@ export default {
     filter: {
       handler() {
         this.itemIndex = [];
-          this.totalItems= 0;
-          this.options.page = 1;
+        this.totalItems = 0;
+        this.options.page = 1;
         this.$fetch();
       },
       deep: true,
@@ -198,87 +242,71 @@ export default {
   methods: {
     getValueFromType(item, type) {
       if (!item.features[0].types) {
-        return "-";
+        return '-';
       }
-      console.log(type);
 
       return item.features[0].types
         .filter((t) => t.hierarchy === type)
         .map((x) => x.label)
-        .join(", ");
+        .join(', ');
     },
-    getFirstTypeByKeyword(types,keyword) {
-      let type = types.find((x) => x.hierarchy.split(" > ")[0] === keyword)
+    getFirstTypeByKeyword(types, keyword) {
+      const type = types.find((x) => x.hierarchy.split(' > ')[0] === keyword);
 
-      type.supertype = type.hierarchy.split(" > ")[0];
-      type.subtype = type.hierarchy.split(" > ")[1];
+      [type.supertype, type.subtype] = type.hierarchy.split(' > ');
       return type;
-    }
-    ,
+    },
     getOrderedTypes(types) {
       if (!types) return {};
-      const newTypes = types
+      return types
         .map((type) => {
-          var t = {};
-          t.type = type.hierarchy.split(" > ")[0];
-          t.subtype = type.hierarchy.split(" > ")?.[1];
-          t.id = type.identifier.split("/").splice(-1)[0];
+          const t = {};
+          [t.type, t.subtype] = type.hierarchy.split(' > ');
+          [t.id] = type.identifier.split('/').splice(-1);
           t.label = type.label;
           t.value = type.value;
           t.unit = type.unit;
           return t;
         })
         .reduce((accumulator, element) => {
-          if (!(element.type in accumulator))
-            accumulator[element.type] = [element];
+          if (!(element.type in accumulator)) accumulator[element.type] = [element];
           else accumulator[element.type].push(element);
           return accumulator;
         }, {});
-      return newTypes;
     },
   },
-  computed: {
-    ...mapGetters("app", [
-      "getTypesBySystemClass",
-      "getIconBySystemClass",
-      "getLabelBySystemClass",
-      "getCRMClassBySystemClass",
-      "getSortColumnByPath",
-      "getSystemClassForFilter",
-      "getFilterList",
-    ]),
-    cssVars() {
-      let colCount = 2;
-      if (this.$vuetify.breakpoint.name === "xs") colCount = 1;
 
-      return {
-        "--column-count": colCount,
-      };
-    },
-  },
 };
 </script>
 <style scoped>
 .list-element:hover {
 }
+
 .list-element {
   transition: all 0.1s;
 }
 
 .card-columns {
-  -moz-column-count: 4;
-  -webkit-column-count: 4;
+  -moz-column-count: var(--column-count);
+  -webkit-column-count: var(--column-count);
   column-count: var(--column-count);
+
 }
 
 .card-column {
   -webkit-column-break-inside: avoid; /* Chrome, Safari */
   page-break-inside: avoid; /* Theoretically FF 20+ */
   break-inside: avoid-column; /* IE 11 */
+  overflow: hidden; /* Firefox */
+
 }
-.artifact-title{
+
+.artifact-title {
   transition: all 100ms linear !important;
 }
 
+.borderBottom {
+  border-bottom: 1px solid grey !important
+}
 
 </style>
