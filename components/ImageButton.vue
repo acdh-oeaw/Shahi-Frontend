773d8b2dc20b1eded67ac5c507f66028c248134c
Andi <andreas@olschnoegger.at>
2021-09-14 08:20:23 +0200

    move image buttons to own component

---
+++ components/ImageButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-btn
+    :to="to"
+    @click="$emit('click')"
+    class="navigation-btn"
+  >
+    <v-icon v-if="icon" class="btn-icon mb-3" size="140">
+      {{icon}}
+    </v-icon>
+    <div style="height: 100%; width: 100%" class="d-flex align-end justify-end" >
+    <p class="d-flex align-start  text-subtitle-1" >
+      <slot></slot>
+    </p>
+    </div>
+  </v-btn>
+</template>
+
+<script>
+export default {
+name: "ImageButton",
+  props:['icon','to'],
+}
+</script>
+
+<style scoped>
+.navigation-btn {
+  overflow: hidden;
+  width: -moz-calc(100% - 20px);
+  width: -webkit-calc(100% - 20px);
+  width: calc(100% - 20px);
+  margin: 10px 10px;
+  background-color: rgba(255, 255, 255, 0.5) !important;
+  transition: all 100ms linear;
+  min-height: 80px;
+
+}
+
+.btn-icon{
+  left: 3%;
+  position: absolute;
+
+  opacity: 30%;
+  width: 130px;
+  transition: all 100ms linear;
+}
+.navigation-btn >>> .v-btn__content {
+  position: relative;
+  max-width: 350px;
+  height: 100%;
+}
+
+
+</style>
--- components/ImageNavigation.vue
+++ components/ImageNavigation.vue
@@ -25,35 +25,15 @@
           </div>
         </v-col>
         <v-col cols="12">
-          <v-btn
-            to="/"
-            class="navigation-btn text-body-1"
-          >
-            <v-img class="btn-icon" src="http://simpleicon.com/wp-content/uploads/map-marker-1.png" />
+          <ImageButton to="/" icon="mdi-map-marker">Discover the Kindoms</ImageButton>
 
-            <p class="ma-0 pa-0">
-              Discover Shahi Kingdoms
-            </p>
-          </v-btn>
         </v-col>
 
         <v-col cols="12" sm="6">
-          <v-btn class="navigation-btn text-body-1" to="/list?codes=artifact">
-            <v-img class="btn-icon" src="https://static.thenounproject.com/png/3156130-200.png" />
-            <p class="ma-0 pa-0">
-              Enter Database
-            </p>
-          </v-btn>
+          <ImageButton to="/team" icon="$artifact">Enter Database</ImageButton>
         </v-col>
         <v-col cols="12" sm="6">
-          <v-btn class="navigation-btn text-body-1" to="/list?codes=artifact">
-            <v-icon class="btn-icon mb-3" size="140">
-              mdi-account-group
-            </v-icon>
-            <p class="ma-0 pa-0">
-              Meet the Team
-            </p>
-          </v-btn>
+          <ImageButton to="/team" icon="mdi-account-group">Meet the Team</ImageButton>
         </v-col>
       </v-row>
     </v-img>
@@ -126,41 +106,6 @@ export default {
 
 <style scoped>
 
-.navigation-btn {
-  overflow: hidden;
-  width: -moz-calc(100% - 20px);
-  width: -webkit-calc(100% - 20px);
-  width: calc(100% - 20px);
-  margin: 10px 10px;
-  background-color: rgba(255, 255, 255, 0.5) !important;
-  transition: all 100ms linear;
-  min-height: 80px;
-
-}
-
-.btn-icon{
-  left: 3%;
-  position: absolute;
-
-  opacity: 30%;
-  width: 130px;
-  transition: all 100ms linear;
-}
-
-.navigation-btn p{
-  position: absolute;
-
-  bottom: 15px;
-  right: 0;
-}
-.navigation-btn:hover .btn-icon{
-  opacity: 50%;
-}
-
-.navigation-btn:hover {
-  transform: scale(1.05);
-  background-color: rgba(255, 255, 255, 1) !important;
-}
 
 .navigation-btns{
   max-width: 600px;
@@ -171,11 +116,5 @@ export default {
   margin: auto;
 }
 
-.navigation-btn >>> .v-btn__content {
-  position: relative;
-  max-width: 350px;
-  height: 100%;
-
-}
 
 </style>
