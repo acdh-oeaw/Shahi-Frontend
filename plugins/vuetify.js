01e065d75fc70b0b0f2d32651e5fa456aeb6c4e8
Andi <andreas@olschnoegger.at>
2021-09-20 10:22:47 +0200

    change color theme

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,10 +1,30 @@
-import IconArtifact from "~/components/icons/IconArtifact.vue"
+import colors from 'vuetify/es5/util/colors';
+import IconArtifact from '~/components/icons/IconArtifact.vue';
+
 export default {
   icons: {
     values: {
       artifact: {
         component: IconArtifact,
-      }
-    }
-  }
-}
+      },
+    },
+  },
+  theme: {
+    light: true,
+    themes: {
+      light: {
+        'soya-bean': '#655a4b',
+        peat: '#716a56',
+        'gray-nickel': '#bcbbb8',
+        armadillo: '#423f35',
+        primary: '#423f35',
+        accent: '#655a4b',
+        secondary: '#423f35',
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3,
+      },
+    },
+  },
+};
