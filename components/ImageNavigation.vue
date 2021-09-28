0e5732bf4ade8b38800225602f957d904add7061
Andi <andreas@olschnoegger.at>
2021-09-28 08:57:00 +0200

    linting

--- components/ImageNavigation.vue
+++ components/ImageNavigation.vue
@@ -1,48 +1,45 @@
 <template>
-  <div>
-    <v-img
-      :height="$vuetify.breakpoint.xs ? '98vh' : '100%'"
-      min-height="30vh"
-      style="background: linear-gradient(to bottom, #727076 0%, #dbdad8 100%);"
-      :src="$vuetify.breakpoint.xs ? '' : '/header.png'"
+  <v-img
+    :height="$vuetify.breakpoint.xs ? '98vh' : '100%'"
+    style="background: linear-gradient(to bottom, #727076 0%, #dbdad8 100%);"
+    :src="$vuetify.breakpoint.xs ? '' : '/header.png'"
+  >
+    <v-row
+      no-gutters
+      class="d-flex justify-center"
+      :class="{'navigation-btns': $vuetify.breakpoint.smAndUp}"
     >
-      <v-row
-        no-gutters
-        class="d-flex justify-center"
-        :class="{'navigation-btns': $vuetify.breakpoint.smAndUp}"
+      <v-col
+        cols="12"
+        sm="6"
+        class="d-flex align-center align-sm-start justify-center justify-sm-end "
       >
-        <v-col
-          cols="12"
-          sm="6"
-          class="d-flex align-center align-sm-start justify-center justify-sm-end "
+        <div
+          class="image-header text-h3 text-center pa-2 mt-10 hidden-sm-and-up"
         >
-          <div
-            class="image-header text-h3 text-center pa-2 mt-10 hidden-sm-and-up"
-          >
-            <p class="text-h3">
-              Shahi Kingdoms Database
-            </p>
-          </div>
-        </v-col>
-        <v-col cols="12">
-          <ImageButton :to="{'name':'index','path':'/','params':{'showMap':true}}" icon="mdi-map-marker">
-            Discover the Kindoms
-          </ImageButton>
-        </v-col>
+          <p class="text-h3">
+            Shahi Kingdoms Database
+          </p>
+        </div>
+      </v-col>
+      <v-col cols="12">
+        <ImageButton :to="{'name':'index','path':'/','params':{'showMap':true}}" icon="mdi-map-marker">
+          Discover the Kindoms
+        </ImageButton>
+      </v-col>
 
-        <v-col cols="12" sm="6">
-          <ImageButton to="/list?codes=artifact" icon="$artifact">
-            Enter Database
-          </ImageButton>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <ImageButton to="/team" icon="mdi-account-group">
-            Meet the Team
-          </ImageButton>
-        </v-col>
-      </v-row>
-    </v-img>
-  </div>
+      <v-col cols="12" sm="6">
+        <ImageButton to="/list?codes=artifact" icon="$artifact">
+          Enter Database
+        </ImageButton>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <ImageButton to="/team" icon="mdi-account-group">
+          Meet the Project Team
+        </ImageButton>
+      </v-col>
+    </v-row>
+  </v-img>
 </template>
 
 <script>
@@ -114,7 +111,7 @@ export default {
 
 <style scoped>
 
-.navigation-btns{
+.navigation-btns {
   max-width: 600px;
   position: absolute;
   bottom: 4vw;
