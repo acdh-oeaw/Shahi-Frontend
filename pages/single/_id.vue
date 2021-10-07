58377653ac466b0afc789839204f7e807ede28ad
Andi <andreas@olschnoegger.at>
2021-10-07 10:02:01 +0200

    restructuring page

--- pages/single/_id.vue
+++ pages/single/_id.vue
@@ -1,89 +1,87 @@
 <template>
   <v-sheet v-if="!loading">
-    <v-sheet class="secondary darken-1 pa-5 pa-sm-10">
-      <div class="page-content">
-      <v-row  no-gutters>
-        <v-col cols="12" md="6" class="d-flex align-center">
-          <v-sheet max-width="400px" color="transparent" >
-            <p class="text-h3">
-              {{ item.features[0].properties.title }}
-            </p>
-            <p v-if="types.Period" class="text-h5 text--secondary mt-n3">
-              {{ types.Period[0].label }}
-            </p>
-            <p v-else class="text--secondary mt-n3">
-              <span v-if="item.features[0].when.timespans[0].start.earliest">
-                from {{ item.features[0].when.timespans[0].start.earliest
-                .split("-")[0]
-                .replace(/^0+/, "") }}</span>
-              <span v-if="item.features[0].when.timespans[0].end.earliest">
-                to {{ item.features[0].when.timespans[0].end.earliest
-                .split("-")[0]
-                .replace(/^0+/, "") }}</span>
-            </p>
-            <p v-if="item.features[0].description">
-              {{ item.features[0].description[0].value }}
-            </p>
-          </v-sheet>
-        </v-col>
-        <v-col cols="12" md="6" >
+    <v-sheet class="secondary darken-1 ">
+      <div class="page-content pa-5 pa-sm-10">
+        <v-row no-gutters>
+          <v-col cols="12" md="6" class="d-flex align-center">
+            <v-sheet max-width="400px" color="transparent">
+              <p class="text-h3">
+                {{ item.features[0].properties.title }}
+              </p>
+              <p v-if="types.Period" class="text-h5 text--secondary mt-n3">
+                {{ types.Period[0].label }}
+              </p>
+              <p v-else class="text--secondary mt-n3">
+                <span v-if="item.features[0].when.timespans[0].start.earliest">
+                  from {{ item.features[0].when.timespans[0].start.earliest
+                    .split("-")[0]
+                    .replace(/^0+/, "") }}</span>
+                <span v-if="item.features[0].when.timespans[0].end.earliest">
+                  to {{ item.features[0].when.timespans[0].end.earliest
+                    .split("-")[0]
+                    .replace(/^0+/, "") }}</span>
+              </p>
+              <p v-if="item.features[0].description">
+                {{ item.features[0].description[0].value }}
+              </p>
+            </v-sheet>
+          </v-col>
+          <v-col cols="12" md="6">
             <ImageViewer />
-        </v-col>
-
-      </v-row>
+          </v-col>
+        </v-row>
       </div>
     </v-sheet>
     <v-sheet class="page-content mt-10 px-5 pa-sm-10">
-        <p class="text-overline">
-          Details
-        </p>
-          <v-row v-if="!!item.features[0].types" no-gutters class="text--secondary">
-            <v-col v-for="i in [0,1]" :key="i" cols="12" sm="5">
-              <v-row
-                v-for="(typeGroup, index) in cutInHalf(Object.values(types))[i]"
-                :key="index"
-                no-gutters
-              >
-                <v-col cols="auto" class="mr-1">
-                  <p v-if="typeGroup[0].supertype === 'Material'" class="font-weight-bold">
-                    Materials:
-                  </p>
-                  <p v-else class="font-weight-bold">
-                    {{ typeGroup[0].supertype }}:
-                  </p>
-                </v-col>
-                <v-col cols="auto">
-                  <p style="max-width: 400px">
-                    {{ typeGroup.map(x => x.label).join(', ') }}
-                  </p>
-                </v-col>
-              </v-row>
-            </v-col>
-          </v-row>
-          <p class="text-overline">
-            Relations
-          </p>
+      <p class="text-overline">
+        Details
+      </p>
+      <v-row v-if="!!item.features[0].types" no-gutters class="text--secondary">
+        <v-col v-for="i in [0,1]" :key="i" cols="12" sm="5">
           <v-row
-            class="text--secondary"
-            v-for="(relation,index) in item.features[0].relations
-                  .filter(x => x.relationType !== 'crm:P2 has type')"
+            v-for="(typeGroup, index) in cutInHalf(Object.values(types))[i]"
             :key="index"
             no-gutters
           >
-            {{ relation.relationType.split(' ').slice(1).join(' ') }}:
-            {{ relation.label }}
-            {{ relation.relationType }}
+            <v-col cols="auto" class="mr-1">
+              <p v-if="typeGroup[0].supertype === 'Material'" class="font-weight-bold">
+                Materials:
+              </p>
+              <p v-else class="font-weight-bold">
+                {{ typeGroup[0].supertype }}:
+              </p>
+            </v-col>
+            <v-col cols="auto">
+              <p style="max-width: 400px">
+                {{ typeGroup.map(x => x.label).join(', ') }}
+              </p>
+            </v-col>
           </v-row>
+        </v-col>
+      </v-row>
+      <p class="text-overline">
+        Relations
+      </p>
+      <v-row
+        v-for="(relation,index) in item.features[0].relations
+          .filter(x => x.relationType !== 'crm:P2 has type')"
+        :key="index"
+        class="text--secondary"
+        no-gutters
+      >
+        {{ relation.relationType.split(' ').slice(1).join(' ') }}:
+        {{ relation.label }}
+        {{ relation.relationType }}
+      </v-row>
     </v-sheet>
-    <v-sheet class="px-10">
-      </v-sheet>
+    <v-sheet class="px-10" />
 
     <v-sheet class="page-content map pa-5 pa-sm-10">
-    <qmap
-      v-if="!loading"
-      :geojsonitems="[item]"
-      style="height: 500px"
-    />
+      <qmap
+        v-if="!loading"
+        :geojsonitems="[item]"
+        style="height: 500px"
+      />
     </v-sheet>
   </v-sheet>
 </template>
@@ -221,6 +219,5 @@ export default {
 .map{
   margin-top: 120px;
 }
-
 
 </style>
