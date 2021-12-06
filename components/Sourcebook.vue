156101b6c7d165acd1f63bec56890fe674221d6c
schnoeggi <andreas@olschnoegger.at>
2021-12-06 10:21:06 +0100

    move navigation to sourcebook component

--- components/Sourcebook.vue
+++ components/Sourcebook.vue
@@ -1,20 +1,74 @@
 <template>
   <div class="pdf-viewer">
-    <slot></slot>
+    <div class="navigator black d-flex justify-space-between">
+      <div
+        v-if="page !== 0"
+        class="pagination secondary clickable text-body-1"
+        @click="page = page - 1"
+      >
+        <v-icon large> mdi-chevron-left </v-icon>
+      </div>
+      <div v-else class="pagination grey text-body-1">
+        <v-icon large> mdi-chevron-left </v-icon>
+      </div>
 
-    <div id="pdf-viewer" 
-      style="width:100%;height:100%"></div>
+      <div style="height: 100%; width: 2px" class="secondary darken-1" />
+      <v-menu bottom 
+      rounded="0"
+      transition="slide-y-transition" 
+        offset-y  content-class="menu-content elevation-0"
+     >
+        <template v-slot:activator="{ on, attrs }">
+              
+        <div
+        v-on="on"
+          class="
+          clickable
+            secondary
+            text-body-1
+            d-flex
+            justify-center
+            align-center
+            px-15
+            text-center
+          "
+        >
+          {{ pages[page].title }}
+        </div>
+        </template>
+        <v-list style="max-width:750px;" class="secondary lighten-1">
+          <v-list-item v-for="(item,index) in pages" :key="index" @click="page=index">
+          <v-list-item-title :class="{'font-weight-bold':index===page}">  {{item.title}}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <div style="height: 100%; width: 1px" class="secondary darken-1" />
+
+      <div
+        v-if="page !== pages.length - 1"
+        class="pagination secondary clickable text-body-1"
+        @click="page = page + 1"
+      >
+        <v-icon large> mdi-chevron-right </v-icon>
+      </div>
+      <div v-else class="pagination grey lighten-1 text--disabled text-body-1">
+        <v-icon large> mdi-chevron-right </v-icon>
+      </div>
+    </div>
+
+    <div id="pdf-viewer" style="width: 100%; height: 100%"></div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import PDFObject from 'pdfobject';
+import axios from "axios";
+import PDFObject from "pdfobject";
 export default {
-  name: 'Sourcebook',
-  props: ['page'],
+  name: "Sourcebook",
+  props: ["pages"],
   data() {
     return {
+      page: 0,
       pdf: undefined,
     };
   },
@@ -28,10 +82,8 @@ export default {
   },
   methods: {
     async getPdf() {
-      console.log(this.page.url);
-
-      const p = await axios.get(this.page.url, {
-        responseType: 'blob',
+      const p = await axios.get(this.pages[this.page].url, {
+        responseType: "blob",
       });
       this.pdf = URL.createObjectURL(p.data);
 
@@ -44,11 +96,41 @@ export default {
 <style scoped>
 .pdf-viewer {
   margin: auto;
-  width:100%;
-  min-height:500px;
+  width: 100%;
+  min-height: 500px;
   position: relative;
 }
 
-.pdfobject-container { height: 30rem; border: 1rem solid rgba(0,0,0,.1); }
+.pdfobject-container {
+  height: 30rem;
+  border: 1rem solid rgba(0, 0, 0, 0.1);
+}
 
+.navigator {
+  max-width: 750px;
+  height: 60px;
+  left: 0;
+  right: 0;
+  position: absolute;
+  z-index: 4000;
+}
+
+.pagination {
+  user-select: none;
+
+  min-width: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 100ms ease;
+}
+
+.clickable:hover {
+  filter: opacity(0.9);
+}
+.menu-content {
+  transform: translateX(-61px);
+  max-height: 500px;
+  
+}
 </style>
--- pages/sourcebook/index.vue
+++ pages/sourcebook/index.vue
@@ -9,42 +9,8 @@
     </section>
     <section>
       <div class="d-flex">
-        <sourcebook v-if="!!sourcebook" class="sourcebook-page" :page="sourcebook.depictions[page]">
-          <div class="navigator black d-flex justify-space-between  ">
-            <div v-if="page !== 0" class="pagination secondary clickable text-body-1" @click="page=page-1">
-              <v-icon large>
-                mdi-chevron-left
-              </v-icon>
-            </div>
-            <div v-else class="pagination  grey text-body-1">
-              <v-icon large>
-                mdi-chevron-left
-              </v-icon>
-            </div>
-
-            <div style="height:100%;width:2px;" class="secondary darken-1" />
-
-            <div class="secondary text-body-1 d-flex justify-center align-center px-15 text-center">
-              {{ sourcebook.depictions[page].title }}
-            </div>
-
-            <div style="height:100%;width:1px;" class="secondary darken-1" />
-
-            <div
-              v-if="page !== sourcebook.depictions.length -1"
-              class="pagination secondary clickable text-body-1"
-              @click="page=page+1"
-            >
-              <v-icon large>
-                mdi-chevron-right
-              </v-icon>
-            </div>
-            <div v-else class="pagination grey lighten-1 text--disabled text-body-1">
-              <v-icon large>
-                mdi-chevron-right
-              </v-icon>
-            </div>
-          </div>
+        <sourcebook v-if="!!sourcebook" class="sourcebook-page" :pages="sourcebook.depictions">
+          
         </sourcebook>
       </div>
     </section>
@@ -57,7 +23,6 @@ export default {
   data() {
     return {
       sourcebook: undefined,
-      page: 0,
     };
   },
   async mounted() {
@@ -89,28 +54,4 @@ export default {
   height: 800px;
 }
 
-.navigator {
-  max-width: 750px;
-  height: 60px;
-  left: 0;
-  right: 0;
-  position: absolute;
-  z-index: 4000;
-}
-
-.pagination {
-  user-select: none;
-
-  min-width: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 100ms ease;
-
-}
-
-.clickable:hover {
-  filter: opacity(0.9);
-
-}
 </style>
