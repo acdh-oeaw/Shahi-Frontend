74fffab27e37af0ade9b3eea76a4962f4af8455e
Andi <andreas@olschnoegger.at>
2022-04-06 08:42:45 +0200

    update map

--- components/DataMap.vue
+++ components/DataMap.vue
@@ -5,18 +5,18 @@
         :size="200"
         color="primary"
         indeterminate
-      ></v-progress-circular>
+      />
     </div>
 
     <qmap :geojsonitems="visibleArtifacts" />
 
-    <div  class="timeline elevation-4 relative" :class="{show:useTimeline}">
+    <div class="timeline elevation-4 relative" :class="{show:useTimeline}">
       <v-btn v-if="useTimeline" icon @click="useTimeline = false">
         <v-icon size="23">
           mdi-close
         </v-icon>
       </v-btn>
-      <v-btn v-else icon @click="useTimeline = true" title="configure timeline">
+      <v-btn v-else icon title="configure timeline" @click="useTimeline = true">
         <v-icon size="23">
           mdi-timeline-clock-outline
         </v-icon>
@@ -38,12 +38,16 @@
           </v-col>
         </v-row>
       </transition>
-    
     </div>
-    <div :title="showPolygons ? 'hide polygons' : 'show polygons'" @click="showPolygons = !showPolygons" class="showploygons elevation-4" :class="{active: !showPolygons}">
-    <v-icon size="23">
-          mdi-vector-polygon
-        </v-icon>
+    <div
+      :title="showPolygons ? 'hide polygons' : 'show polygons'"
+      class="showploygons elevation-4"
+      :class="{active: !showPolygons}"
+      @click="showPolygons = !showPolygons"
+    >
+      <v-icon size="23">
+        mdi-vector-polygon
+      </v-icon>
     </div>
   </div>
 </template>
@@ -59,7 +63,7 @@ export default {
   },
   data() {
     return {
-      showPolygons:true,
+      showPolygons: true,
       useTimeline: false,
       loading: false,
       toggler: 1,
@@ -92,25 +96,26 @@ export default {
       return parseInt(date.split('-')[0], 10);
     },
     async loadArtifacts() {
+      if (this.loading) return;
       this.loading = true;
       let localItems = [];
       const p = await this.$api.Entities.get_api_0_3_query_({
         view_classes: this.getQuery?.view_classes,
         search: this.getQuery?.search,
-        page: 1,
+        limit: 99999,
       });
-
-      await Promise.all(Array.from({ length: p.body.pagination.totalPages }, async (x, i) => {
-        const q = await this.$api.Entities.get_api_0_3_query_({
-          view_classes: this.getQuery?.view_classes,
-          search: this.getQuery?.search,
-          page: i,
-        });
-        localItems = [...localItems, ...q.body.results.map((x) => x.features[0])];
+      localItems = [...localItems, ...p.body.results.map((x) => x.features[0])];
+      localItems = localItems.map((x) => ({
+        ...x,
+        properties: {
+          ...x.properties,
+          link: `/single/${x['@id'].split('/').pop()}`,
+          linkTitle: `<span class="text-body-1">${x.properties.title}</span> <br/> <span class="map-detail-link text-body-2">Details</span>`,
+        },
       }));
       this.loading = false;
       this.items = localItems;
-      console.log(this.items)
+      console.log(this.items);
     },
   },
   computed: {
@@ -118,7 +123,7 @@ export default {
     ...mapGetters('query', ['getQuery']),
     visibleArtifacts() {
       const year = (this.time + 1) * 100;
-      let localItems = this.items.filter(x => this.showPolygons || x?.geometry?.type !== 'Polygon')
+      const localItems = this.items.filter((x) => this.showPolygons || x?.geometry?.type !== 'Polygon');
       if (!this.useTimeline) return localItems;
       return localItems
         .filter((x) => this.dateStringToYear(x.when?.timespans?.[0]?.start.earliest) <= year)
@@ -172,33 +177,36 @@ export default {
   top: 0;
   left: 0;
 }
-.showploygons{
+
+.showploygons {
   position: absolute;
   padding: 5px;
   background-color: white;
-  width:35px;
-  height:35px;
+  width: 35px;
+  height: 35px;
   z-index: 400;
-  right:10px;
-  top:55px;
-  cursor:pointer;
-  transition:all linear 100ms;
+  right: 10px;
+  top: 55px;
+  cursor: pointer;
+  transition: all linear 100ms;
 }
-a.showploygons{
-  top:0;
-  left:0;
-  right:0;
-  bottom:0;
+
+a.showploygons {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   position: absolute;
   background-color: green;
 }
-.showploygons.active{
+
+.showploygons.active {
   filter: brightness(90%);
 }
-.showploygons:hover{
-filter: brightness(95%);
-}
 
+.showploygons:hover {
+  filter: brightness(95%);
+}
 
 .timeline {
   position: absolute;
@@ -207,39 +215,54 @@ filter: brightness(95%);
   top: 10px;
   right: 10px;
   z-index: 401;
-  width:35px;
-  height:35px;
+  width: 35px;
+  height: 35px;
   transition: all ease-in-out 100ms;
 }
 
-.show{
+.show {
 
   height: 120px;
   width: 400px;
 }
+
 .timeline button {
   position: absolute;
-  top:0;
-  right:0;
+  top: 0;
+  right: 0;
 }
 
 .timelineTransition-enter-active {
   animation: finished 200ms reverse;
 }
+
 .timelineTransition-leave-active {
   animation: finished 100ms;
 }
+
 @keyframes finished {
-  0% { opacity: 1;}
-  100% { opacity: 0;}
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
 }
 
-.loadScreen{
+.loadScreen {
   position: absolute;
-  width:100%;
-  height:100%;
-  background-color:rgba(0,0,0,0.2);
-  z-index:4991;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 4991;
 }
 
+</style>
+<style>
+.map-detail-link{
+  transition: all 100ms ease-in-out;
+}
+.map-detail-link:hover{
+  font-weight: bold;
+}
 </style>
