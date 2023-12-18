8d4985da8f47e5189e80cd0d77e3f264fa75b68f
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2023-12-18 14:17:31 +0100

    Basic search in title and description

--- mixins/filterItemsMixin.js
+++ mixins/filterItemsMixin.js
@@ -3,8 +3,18 @@ export default {
     filteredBaseItems: function () {
       const items = this.items;
       const query = this.$route.query;
-      console.log('query', query);
       const view_classes = query?.view_classes;
+
+      let searchValue = getSearchValue(query);
+
+      if(searchValue && searchValue.length > 0) {
+        return items.filter(item => {
+          const inTitle = item.features[0].properties.title.toLowerCase().includes(searchValue.toLowerCase());
+          const inDescription = item.features[0].descriptions.find(desc => {
+            return desc?.value?.toLowerCase().includes(searchValue.toLowerCase())}) !== undefined;
+          return inTitle || inDescription;
+        });
+      }
 
       console.log('view_classes', view_classes);
       if (view_classes) {
@@ -15,7 +25,32 @@ export default {
           return false;
         });
       }
-      else return items;
+      
+      return items;
     },
+  }
+}
+
+
+/**
+ * Parses the search query parameter to extract the search value.
+ *
+ * The search query can be a string or array. This normalizes it to always
+ * return a string search value.
+ *
+ * @param {Object} query - The route query parameters object
+ * @returns {string} The extracted search value
+ */
+function getSearchValue(query) {
+  const search = query?.search;
+  if (!search) return "";
+
+  if (Array.isArray(search)) {
+    if(search.length < 1) return "";
+    const parsedSearch = search.map(s => {
+      return JSON.parse(s)});
+    return parsedSearch[0].entityName[0].values[0];
+  } else {
+    return JSON.parse(search).entityName[0].values[0];
   }
 }
