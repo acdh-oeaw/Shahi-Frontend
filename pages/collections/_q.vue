b6c024a4198a26fd7f74ad8b5b4e2e3913241250
Mocca101 <49754596+Mocca101@users.noreply.github.com>
2024-01-02 19:23:30 +0100

    use local images in collections

--- pages/collections/_q.vue
+++ pages/collections/_q.vue
@@ -35,54 +35,11 @@
 import list from "~/components/lists/list";
 import Otherlist from "@/components/lists/otherlist";
 import GalleryList from "@/components/lists/galleryList";
+import axios from "axios";
 
 export default {
   name: "q_.vue",
   components: {GalleryList, Otherlist, list},
-  // async asyncData({ $api, route }) {
-  //   const items = await $api.Entities.get_api_0_3_type_entities__id__({
-  //     id_: route.params.q,
-  //     limit: 0,
-  //   });
-
-  //   const sortedItems = items.body.results.sort((a, b) => {
-  //     const aTitle = a.features[0]?.properties?.title;
-  //     const bTitle = b.features[0]?.properties?.title;
-
-  //     if(aTitle === undefined) return -1;
-  //     if(bTitle === undefined) return 1;
-
-  //     if (sortDesc[0]) {
-  //       return aTitle < bTitle ? 1 : -1;
-  //     } else {
-  //       return aTitle > bTitle ? 1 : -1;
-  //     }
-  //   });
-
-  //   // Fetch descriptions for all items
-  //   const descriptions = await Promise.all(
-  //     sortedItems.map(item =>
-  //       $api.Entities.get_api_0_3_entity__id__({
-  //         id_: item.features[0].depictions?.[0]['@id'].split('/').at(-1),
-  //         show: 'description'
-  //       })
-  //     )
-  //   );
-
-  //   // Map descriptions to items
-  //   sortedItems.forEach((item, index) => {
-  //     if(item?.body?.features?.[0]?.descriptions?.[0]?.value) {
-  //       item.description = item?.body?.features?.[0]?.descriptions?.[0]?.value;
-  //     }
-  //   });
-
-  //   return {
-  //     items: sortedItems,
-  //     itemIndex: items.body.pagination.index,
-  //     totalItems: sortedItems.length,
-  //     notFound: false
-  //   };
-  // },
   async fetch() {
     this.loading = true;
     const {
@@ -97,6 +54,71 @@ export default {
 
       this.items = p.body.results;
 
+      const depictionsInfo = (await axios.get('https://shahi.openatlas.eu/api/0.4/licensed_file_overview/')).data;
+      
+      this.sortItems(sortDesc);
+
+      const descriptions = await Promise.all(
+        this.items.map(async item => {
+            const returnObj = {};
+            const depictionID = item.features[0].depictions?.[0]['@id'].split('/').pop();
+            if(!depictionID) return null;
+
+            if (depictionsInfo[depictionID] && depictionsInfo[depictionID].extension) {
+              const extension = depictionsInfo[depictionID].extension;
+              returnObj.url = `/entity_files/${depictionID + extension}`
+            } else {
+              log(`Can't read depiction ${depictionID}`);
+              log(depictionsInfo[depictionID]);
+            }
+
+            try {              
+              const description = await this.$api.Entities.get_api_0_3_entity__id__({
+                id_: depictionID,
+                show: 'description'
+              });
+              returnObj.description = description?.body?.features?.[0]?.descriptions?.[0]?.value || null;
+            } catch (error) {
+              if (error.response && error.response.status === 502) {
+                console.log(`Bad Gateway error for id: ${depictionID}`);
+              } else {
+                console.log(`Error for id: ${depictionID}`)
+                console.log(error);
+              }
+              returnObj.description = null;
+            }
+            return returnObj;
+        })
+      );
+
+      if(descriptions.length !== this.items.length) {
+        console.log('descriptions length', descriptions.length);
+        console.log('items length', this.items.length);
+        throw new Error('descriptions length !== items length');
+      }
+
+      // Map descriptions to items
+      descriptions.forEach((item, index) => {
+        if(item?.description) {
+          this.items[index].features[0].depictions[0].description = item?.description;
+        }
+        if(item?.url) {
+          this.items[index].features[0].depictions[0].url = item.url;
+        }
+      });
+
+      this.notFound = false;
+
+      this.itemIndex = p.body.pagination.index;
+      this.totalItems = this.items.length;
+    } catch (err) {
+      console.log(err);
+      this.notFound = true;
+    }
+    this.loading = false;
+  },
+  methods: {
+    sortItems(sortDesc) {
       this.items.sort((a, b) => {
         const aTitle = a.features[0]?.properties?.title;
         const bTitle = b.features[0]?.properties?.title;
@@ -110,53 +132,7 @@ export default {
           return aTitle > bTitle ? 1 : -1;
         }
       });
-
-
-      const descriptions = await Promise.all(
-        this.items.map(async item => {
-            const depictionID = item.features[0].depictions?.[0]['@id'].split('/').at(-1);
-            if(!depictionID) return null;
-            try {
-              
-              const description = await this.$api.Entities.get_api_0_3_entity__id__({
-                id_: depictionID,
-                show: 'description'
-              });
-              return description;
-            } catch (error) {
-              if (error.response && error.response.status === 502) {
-                console.log(`Bad Gateway error for id: ${depictionID}`);
-              } else {
-                console.log(`Error for id: ${depictionID}`)
-                console.log(error);
-              }
-              return null;
-            }
-        })
-      );
-
-      if(descriptions.length !== this.items.length) {
-        console.log('descriptions length', descriptions.length);
-        console.log('items length', this.items.length);
-        throw new Error('descriptions length !== items length');
-      }
-
-      // Map descriptions to items
-      descriptions.forEach((item, index) => {
-        if(item?.body?.features?.[0]?.descriptions?.[0]?.value) {
-          this.items[index].features[0].depictions[0].description = item?.body?.features?.[0]?.descriptions?.[0]?.value;
-        }
-      });
-
-      this.notFound = false;
-
-      this.itemIndex = p.body.pagination.index;
-      this.totalItems = this.items.length;
-    } catch (err) {
-      console.log(err);
-      this.notFound = true;
-    }
-    this.loading = false;
+    },
   },
   data() {
     return {
@@ -172,7 +148,6 @@ export default {
   },
   mounted() {
     //this.$fetch();
-    console.log('items on mounted', this.items)
   },
   watch: {
     '$route.query': {
